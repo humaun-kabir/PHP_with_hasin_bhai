9c69fd251804a447a9f49bf1d08b807716536f6b
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-03-21 14:44:14 +0600

    Create if_else.php

---
+++ Fundamentals of PHP/if_else.php
@@ -0,0 +1,19 @@
+<?php
+$n =13;
+
+if ($n % 2 == 0) {
+	echo "$n is an even number";
+}else{
+	echo "$n is a odd number";
+}
+
+//another example
+
+$alom = 100;
+$shahin = 200;
+
+if ($alom == $shahin) {
+	echo "Alom has same ammount of money as shahin";
+}else if($alom>$shahin){
+	echo "Alom has more money than shahin";
+}
