d6165bf0f95790b5cde44b0fe6fa908efc105457
Humaun Kabir <asmhumaunkabir@gmail.com>
2021-03-21 14:44:06 +0600

    Create number_system.php

---
+++ Fundamentals of PHP/number_system.php
@@ -0,0 +1,11 @@
+<?php
+$n = 12;
+$o = 012;//first digit 0 means octal
+
+printf("The number is %d and %d",$n,$o);//the output is 12 and 10
+
+$h = 0x2B;//hexadecimal number
+
+printf("The binary equivalent of %d is %b \n",1212,1212); //output is The binary equivalent of 1212 is 10010111100
+printf("The hexadecimal equivalent of %d is %x \n",1212,1212); //output is The hexadecimal equivalent of 1212 is 4BC
+printf("The octal equivalent of %d is %o \n",27,27); //output is The octal equivalent of 1212 is 33
